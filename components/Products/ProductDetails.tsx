cd2903a48e5fddd8ec900561a43c17302fb9e67e
Laam Wong <laamwong3@gmail.com>
2022-11-19 06:08:55 +0800

    finish product info

--- components/Products/ProductDetails.tsx
+++ components/Products/ProductDetails.tsx
@@ -1,15 +1,18 @@
-import { Button, Col, Input, Row, Typography } from "antd";
+import { Button, Col, Divider, Input, Row, Typography } from "antd";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import { ProductsProps } from "../../pages/products/[id]";
 import s from "./ProductDetails.module.scss";
 
 const ProductDetails = ({ product }: ProductsProps) => {
+  const [quantity, setQuantity] = useState(1);
+  // console.log(quantity);
+
   return (
     <div className={s.container}>
       <div className={s.content}>
         <Row>
-          <Col xs={24} sm={24} md={24} lg={12} xl={12} xxl={12}>
+          <Col xs={24} sm={24} md={24} lg={24} xl={12} xxl={12}>
             <div className={s.product_img}>
               <Image
                 className={s.img}
@@ -20,40 +23,56 @@ const ProductDetails = ({ product }: ProductsProps) => {
               />
             </div>
           </Col>
-          <Col xs={24} sm={24} md={24} lg={12} xl={12} xxl={12}>
+          <Col xs={24} sm={24} md={24} lg={24} xl={12} xxl={12}>
             <div className={s.product_details}>
               <div className={s.header}>
                 <Typography.Title level={1}>{product.name}</Typography.Title>
+                <Divider />
                 <Typography.Title level={3}>Description:</Typography.Title>
                 <p dangerouslySetInnerHTML={{ __html: product.description }} />
               </div>
               <div className={s.checkout}>
-                <div className={s.form_group}>
+                <div>
                   <div className={s.form_item}>
-                    <Typography.Title level={5}>Price</Typography.Title>
-                    <Typography.Text>
+                    <Typography.Title level={5} className={s.form_item_size}>
+                      Price
+                    </Typography.Title>
+                    <Typography.Text className={s.form_item_size}>
                       {product.price.formatted_with_symbol}
                     </Typography.Text>
                   </div>
                   <div className={s.form_item}>
-                    <Typography.Title level={5}>Status</Typography.Title>
+                    <Typography.Title level={5} className={s.form_item_size}>
+                      Status
+                    </Typography.Title>
                     {product.inventory.available > 0 ? (
-                      <Typography.Text>In Stock</Typography.Text>
+                      <Typography.Text className={s.form_item_size}>
+                        In Stock
+                      </Typography.Text>
                     ) : (
-                      <Typography.Text>Sold Out</Typography.Text>
+                      <Typography.Text className={s.form_item_size}>
+                        Sold Out
+                      </Typography.Text>
                     )}
                   </div>
                   <div className={s.form_item}>
-                    <Typography.Title level={5}>Quantity</Typography.Title>
+                    <Typography.Title level={5} className={s.form_item_size}>
+                      Quantity
+                    </Typography.Title>
                     <Input
+                      className={s.form_item_size}
                       type="number"
                       placeholder="Enter Quantity"
                       min={1}
                       max={product.inventory.available}
-                    ></Input>
+                      value={quantity}
+                      onChange={(e) => setQuantity(Number(e.target.value))}
+                    />
                   </div>
 
-                  <Button>Add to Cart</Button>
+                  <div className={s.button}>
+                    <Button>Add to Cart</Button>
+                  </div>
                 </div>
               </div>
             </div>
